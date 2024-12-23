c1e589aba988dd8125c8c7de790f8278461814c2
Linus Thorsell <thorsell.linus@gmail.com>
2024-12-23 18:05:32 +0100

    Calculate solution.

--- src/solution.cpp
+++ src/solution.cpp
@@ -3,5 +3,13 @@
 using namespace std;
 
 int solution(int number) {
-    return 0;
+    int result = 0;
+
+    for (int i = 0; i < number; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            result += i;
+        }
+    }
+
+    return result;
 }
